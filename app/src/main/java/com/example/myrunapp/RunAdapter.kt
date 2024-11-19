507fb8937a8147b9303e6930486f5b08a10a0061
https://github.com/NomthieVithie/MyRunApp <toodeelicious@gmail.com>
2024-11-19 11:32:27 +0000

    implemented to add the members to the RunAdapter and cleared the error codes.

--- app/src/main/java/com/example/myrunapp/RunAdapter.kt
+++ app/src/main/java/com/example/myrunapp/RunAdapter.kt
@@ -1,4 +1,30 @@
 package com.example.myrunapp
 
-class RunAdapter {
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+data class Runner(val date: String, val morning: Int, val afternoon: Int, val notes: String)
+
+class RunAdapter(private val Runner:List<Runner>) : RecyclerView.Adapter<RunAdapter.viewHolder>() {
+    class viewHolder(view: View) : RecyclerView.ViewHolder(view)
+    {
+        val date: TextView =view.findViewById(R.id.dateOfRun)
+        val morning: TextView =view.findViewById(R.id.morningRun)
+        val noon: TextView =view.findViewById(R.id.afternoonRun)
+        val notes: TextView =view.findViewById(R.id.enterNotes)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): viewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: viewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
 }
